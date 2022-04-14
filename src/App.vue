e8ef5fb298e20e3ad6d7024ad8309d9404338b95
aliafsahnoudeh@gmail.com <aliafsahnoudeh@gmail.com>
2022-04-14 17:26:37 +0200

    App updated

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
+import { provide } from 'vue'
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './views/Main.vue'
+import services from "./services";
+import logicalComponents from "./logical-components";
+
+provide("services", services)
+provide("logical-components", logicalComponents)
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Main />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+@import "./assets/styles/index.scss";
 </style>
